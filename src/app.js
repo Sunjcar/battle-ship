8f0bfecc544acecf857e6bc5c11a03cf968374f8
Raymond Chen <raychen358@gmail.com>
2022-06-24 22:46:41 -0700

    "Set up ship objects"

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const shipFactory = (() => {
+    const Sheldon = {
+        length: 6,
+        hit: 6,
+        sunk: false,
+    } 
+
+    const Kaiden = {
+        length: 5,
+        hit: 5,
+        sunk: false,
+    }
+
+    const subMarine = {
+        length:4,
+        hit: 4,
+        sunk: false
+    }
+
+    const stealthUnit = {
+        length: 2,
+        hit:2,
+        sunk: false
+    }
+
+    const goingMerry = {
+        length:1,
+        hit:1,
+        sunk: false
+    }
+
+})()
+   
+
+   
+export default shipFactory
