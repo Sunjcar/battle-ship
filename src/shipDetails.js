b36f65eb95c888b7977ea829d6fbd0f645dfac6a
Raymond Chen <raychen358@gmail.com>
2022-06-25 21:51:14 -0700

    Ship names and lengths

---
+++ src/shipDetails.js
@@ -0,0 +1,21 @@
+import shipFactory from "./app";
+
+//Battleship Types
+export const SHIP_TYPES = [
+    'Sheldon',
+    'Kaiden',
+    'Destroyer',
+    'Submarine',
+    'Stealth',
+    'GoingMerry',
+]
+    
+export const SHIP_LENGTHS = {
+    Sheldon: 6,
+    Kaiden : 5,
+    Destroyer: 4,
+    Submarine: 3,
+    Stealth: 2,
+    GoingMerry: 1,
+}
+
