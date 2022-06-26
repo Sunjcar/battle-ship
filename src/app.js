3546c7c1ccff80aabf5f62d896c1c9ef24e7617a
Raymond Chen <raychen358@gmail.com>
2022-06-25 23:15:35 -0700

    TDD detect if shit is hit and updates length

--- src/app.js
+++ src/app.js
@@ -1,10 +1,28 @@
 import { SHIP_LENGTHS } from "./shipDetails";
 
 const shipFactory = ((type) => {
+    //Name and size of the ship
     const id = type;
     const length = SHIP_LENGTHS[type];
 
-    return {id, length}
+    //Detect if ship is hit or not
+    const hits = Array(length).fill(null);
+/*     const hit = (i) => {
+        hits[i] = 'hit'
+        return hits
+    }; */
+    const detectHit = () => hits ;
+    const hit = () => {
+        hits.pop();
+        return hits
+    }
+
+    const hitCounter = () => {
+        return length - hits.length
+    }
+
+    // When all spots are hit ship is sunk    
+    return {id, length, hit, detectHit,hitCounter}
 })
    
 
--- src/app.spec.js
+++ src/app.spec.js
@@ -1,13 +1,45 @@
 const shipFactory = require('./app')
 
-describe('Test ship details', () => {
-    describe('details', () => {
-        const ship = shipFactory('GoingMerry');
+describe('details', () => {
+    const ship = shipFactory('GoingMerry');
     test('id', () => {
         expect(ship.id).toBe('GoingMerry')
     })
     test('length', () => {
         expect(ship.length).toBe(1)
     })  
-    })    
+})
+
+
+describe('details', () => {
+    const ship = shipFactory('Destroyer');
+    test('id', () => {
+        expect(ship.id).toBe('Destroyer')
+    })
+    test('length', () => {
+        expect(ship.length).toBe(4)
+    })  
+})
+
+describe('details', () => {
+    const ship = shipFactory('Destroyer');
+    test('1 hit',() => {
+        ship.hit(0)
+        expect(ship.detectHit()).toEqual([null,null,null]);
+    })
+    test('length', () => {
+        expect(ship.hitCounter()).toBe(1)
+    })  
+})
+
+describe('details', () => {
+    const ship = shipFactory('Destroyer');
+    test('2 hits',() => {
+        ship.hit(1)
+        ship.hit(2)
+        expect(ship.detectHit()).toEqual([null,null]);
+    })
+    test('length', () => {
+        expect(ship.hitCounter()).toBe(2)
+    })  
 })
