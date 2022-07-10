e07dfdd5192d034d7fef54a24ffe92a583cb0045
Raymond Chen <raychen358@gmail.com>
2022-07-09 22:02:17 -0700

    Displays game + new font

---
+++ src/view.js
@@ -0,0 +1,94 @@
+import { elements } from "./elements";
+
+const View = (() => {
+    const renderCell = (y, x, status) =>
+    `<div class="grid-cell cell-${y}-${x} ${status}" data-y='${y}' data-x='${x}'></div>`;
+
+  const clearGrid = (parent) => {
+    parent.textContent = '';
+  };
+
+  const renderGrid = (parent, gameboard, type) => {
+    clearGrid(parent);
+    const board = gameboard.getBoard();
+    const length = board.length;
+    let grid = '';
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length; j++) {
+        let status = board[i][j];
+        if (status === null) {
+          status = '';
+        } else if (status.ship) {
+          if (type === 'human') {
+            status = status.ship.id;
+          } else {
+            status = '';
+          }
+        }
+        grid += renderCell(i, j, status);
+      }
+    }
+    parent.insertAdjacentHTML('afterbegin', grid);
+  };
+
+  const renderFleet = (fleet) => {
+    for (const ship in fleet) {
+      //Creates a container for each ship 
+      const container = document.createElement('div');
+      container.classList.add('ship', `${fleet[ship].id}-container`);
+      container.setAttribute('draggable', true);
+      container.dataset.ship = `${fleet[ship].id}`;
+      //Creates individual divs for each ship space
+      let divs = ' ';
+      for (let i = 0; i < fleet[ship].length; i++) {
+        divs += `<div class=${fleet[ship].id} data-index='${i}'></div>`;
+      }
+      container.insertAdjacentHTML('afterbegin', divs);
+      elements.fleetDraggable.prepend(container);
+    }
+  }; renderFleet();
+
+  const autoPlace = () => {
+    elements.startBtn.classList.add('show');
+    elements.fleetInfo.classList.add('hide');
+    elements.fleetInfo.classList.remove('show');
+    elements.fleetDraggable.textContent = '';
+  };
+
+  const startGame = () => {
+    elements.p1Gameboard.classList.toggle('grid-disabled');
+    elements.p2Gameboard.classList.toggle('grid-disabled');
+    elements.p2Gameboard.classList.toggle('show');
+    elements.startBtn.classList.remove('show');
+    elements.autoPlaceBtn.classList.remove('show');
+    elements.fleetContainer.classList.toggle('slide-out');
+  };
+
+  const renderWinner = (winner) => {
+    elements.infoContainer.classList.toggle('show');
+    elements.infoText.textContent = `${winner.toUpperCase()}`;
+  };
+
+  const playAgain = () => {
+    elements.infoContainer.classList.toggle('show');
+    elements.p1Gameboard.classList.toggle('grid-disabled');
+    elements.p2Gameboard.classList.toggle('grid-disabled');
+    elements.p2Gameboard.classList.toggle('hide');
+    elements.p2Gameboard.classList.toggle('show');
+    elements.fleetInfo.classList.toggle('hide');
+    elements.fleetInfo.classList.toggle('show');
+    elements.autoPlaceBtn.classList.add('show');
+    elements.fleetContainer.classList.toggle('slide-in');
+    elements.fleetContainer.classList.toggle('slide-out');
+  };
+
+  return {
+    renderGrid,
+    renderFleet,
+    autoPlace,
+    startGame,
+    renderWinner,
+    playAgain,
+  };
+})();
+export default View
